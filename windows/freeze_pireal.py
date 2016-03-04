a0e431ebebe0dbc3b658138bb4df5dd55cdfba1c
gabo <acostadariogabriel@gmail.com>
2016-03-04 20:04:47 -0300

    Clean up

--- windows/freeze_pireal.py
+++ windows/freeze_pireal.py
@@ -17,43 +17,36 @@
 # You should have received a copy of the GNU General Public License
 # along with Pireal; If not, see <http://www.gnu.org/licenses/>.
 
-import os
-from cx_Freeze import (
-    setup,
-    Executable
-)
-from src import gui
-
-
-def get_packages():
-    packages = []
-    for dir_path, dir_names, filenames in os.walk("src"):
-        if '__pycache__' not in dir_path.split('/')[-1]:
-            if '__init__.py' in filenames:
-                packages.append(dir_path)
-
+from cx_Freeze import setup, Executable
 
 opt = {
     'build_exe': {
-        'includes': ['PyQt4.QtNetwork'],
+        'includes': ['PyQt5.QtNetwork'],
         'include_msvcr': True,
         'include_files': []}}
 
-
 exe = Executable(
-    script="pireal.py",
-    base="Win32GUI",
-    targetName="Pireal.exe",
-    compress=True
+    script="pireal",
+    base='Win32GUI',
+    targetName='Pireal.exe',
+    compress=True,
+    #icon="windows/pireal.ico"
 )
-
 
 setup(
     name="Pireal",
-    version=gui.__version__,
-    author=gui.__author__,
+    version="1.0",
+    author="Gabriel Acosta",
     options=opt,
-    packages=get_packages(),
-    package_data={},
+    packages=[
+        "src",
+        "src.core",
+        "src.gui",
+        "src.gui.dialogs",
+        "src.gui.query_container"
+    ],
+    package_data={
+        "src": ["gui/qml/*"]
+    },
     executables=[exe]
 )
