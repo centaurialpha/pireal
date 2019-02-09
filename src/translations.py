2421f43060a263c3443c4c086e8b5a98a16cda35
centaurialpha <acostadariogabriel@gmail.com>
2019-02-09 17:38:58 -0300

    Started new menu estructure

---
+++ src/translations.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright 2015-2019 - Gabriel Acosta <acostadariogabriel@gmail.com>
+#
+# This file is part of Pireal.
+#
+# Pireal is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# any later version.
+#
+# Pireal is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Pireal; If not, see <http://www.gnu.org/licenses/>.
+
+from PySide2.QtCore import QCoreApplication
+
+tr = QCoreApplication.translate
+
+# Menu
+TR_MENU_DB = tr('Pireal', '&Database')
+TR_MENU_QUERY = tr('Preail', '&Query')
+# TR_MENU_EDIT = tr('Pireal', '&Edit')
+TR_MENU_RELATION = tr('Pireal', '&Relation')
+TR_MENU_HELP = tr('Pireal', '&Help')
+# Menu Database
+TR_MENU_FILE_NEW_DB = tr('Pireal', 'New...')
+TR_MENU_FILE_OPEN_DB = tr('Pireal', 'Open...')
+TR_MENU_FILE_SAVE_DB = tr('Pireal', 'Save...')
+TR_MENU_FILE_SAVE_AS_DB = tr('Pireal', 'Save As...')
+TR_MENU_FILE_CLOSE_DB = tr('Pireal', 'Close')
+# Menu Query
+TR_MENU_QUERY_NEW_QUERY = tr('Pireal', 'New...')
+TR_MENU_QUERY_OPEN_QUERY = tr('Pireal', 'Open...')
+TR_MENU_QUERY_SAVE_QUERY = tr('Pireal', 'Save...')
+TR_MENU_QUERY_SAVE_AS_QUERY = tr('Pireal', 'Save As...')
+TR_MENU_QUERY_EXECUTE = tr('Pireal', 'Execute')
+# Menu Relation
+TR_MENU_RELATION_NEW = tr('Pireal', 'New...')
+TR_MENU_RELATION_DELETE = tr('Pireal', 'Delete')
+TR_MENU_RELATION_LOAD = tr('Pireal', 'Load')
+# Menu Help
+TR_MENU_HELP_HELP = tr('Pireal', 'Help')
+TR_MENU_HELP_REPORT_ISSUE = tr('Pireal', 'Report Issue')
+TR_MENU_HELP_ABOUT = tr('Pireal', 'About Pireal')
+TR_MENU_HELP_ABOUT_QT = tr('Pireal', 'About Qt')
