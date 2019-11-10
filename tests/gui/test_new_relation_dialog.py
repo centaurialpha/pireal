7fcb255b5e2870f0d8e4920fd78e5ffea5a1752d
Gabriel Acosta <acostadariogabriel@gmail.com>
2019-11-10 03:37:50 -0800

    Build travis windows (#104)
    * Jobs to test in Windows

--- tests/gui/test_new_relation_dialog.py
+++ tests/gui/test_new_relation_dialog.py
@@ -1,95 +1,95 @@
-from unittest import mock
-import pytest
+# from unittest import mock
+# import pytest
 
-from PyQt5.QtGui import QStandardItem
+# from PyQt5.QtGui import QStandardItem
 
-from pireal.gui.dialogs.new_relation_dialog import NewRelationDialog
-
-
-@pytest.fixture
-def dialog():
-    module = 'pireal.gui.view.CONFIG.get'
-    with mock.patch(module):
-        w = NewRelationDialog()
-        return w
+# from pireal.gui.dialogs.new_relation_dialog import NewRelationDialog
 
 
-@pytest.mark.skip(reason='Todavía no necesito estos tests')
-@pytest.mark.gui
-@pytest.mark.parametrize(
-    'row_to_insert, expected',
-    [
-        (1, 1),
-        (3, 3),
-        (44, 44)
-    ]
-)
-def test_add_tuple(qtbot, dialog, row_to_insert, expected):
-    qtbot.addWidget(dialog)
-    for _ in range(row_to_insert):
-        dialog._NewRelationDialog__add_tuple()
-    assert dialog._view.model().rowCount() == expected
+# @pytest.fixture
+# def dialog():
+#     module = 'pireal.gui.view.CONFIG.get'
+#     with mock.patch(module):
+#         w = NewRelationDialog()
+#         return w
 
 
-@pytest.mark.skip(reason='Todavía no necesito estos tests')
-@pytest.mark.gui
-@pytest.mark.parametrize(
-    'columns_to_insert, expected',
-    [
-        (1, 3),  # Ya existen 2 columnas por defecto ('Field 1', 'Field 2')
-        (3, 5),
-        (41, 43)
-    ]
-)
-def test_add_column(qtbot, dialog, columns_to_insert, expected):
-    qtbot.addWidget(dialog)
-    for _ in range(columns_to_insert):
-        dialog._NewRelationDialog__add_column()
-    assert dialog._view.model().columnCount() == expected
+# @pytest.mark.skip(reason='Todavía no necesito estos tests')
+# @pytest.mark.gui
+# @pytest.mark.parametrize(
+#     'row_to_insert, expected',
+#     [
+#         (1, 1),
+#         (3, 3),
+#         (44, 44)
+#     ]
+# )
+# def test_add_tuple(qtbot, dialog, row_to_insert, expected):
+#     qtbot.addWidget(dialog)
+#     for _ in range(row_to_insert):
+#         dialog._NewRelationDialog__add_tuple()
+#     assert dialog._view.model().rowCount() == expected
 
 
-@pytest.mark.skip(reason='Todavía no necesito estos tests')
-@pytest.mark.gui
-@pytest.mark.parametrize(
-    'columns_to_insert, to_delete, expected',
-    [
-        (0, 1, 2),
-        (3, 1, 4),
-        (10, 6, 6)
-    ]
-)
-def test_delete_column(qtbot, dialog, columns_to_insert, to_delete, expected):
-    qtbot.addWidget(dialog)
-    for _ in range(columns_to_insert):
-        dialog._NewRelationDialog__add_column()
-    for _ in range(to_delete):
-        dialog._NewRelationDialog__delete_column()
-
-    assert dialog._view.model().columnCount() == expected
+# @pytest.mark.skip(reason='Todavía no necesito estos tests')
+# @pytest.mark.gui
+# @pytest.mark.parametrize(
+#     'columns_to_insert, expected',
+#     [
+#         (1, 3),  # Ya existen 2 columnas por defecto ('Field 1', 'Field 2')
+#         (3, 5),
+#         (41, 43)
+#     ]
+# )
+# def test_add_column(qtbot, dialog, columns_to_insert, expected):
+#     qtbot.addWidget(dialog)
+#     for _ in range(columns_to_insert):
+#         dialog._NewRelationDialog__add_column()
+#     assert dialog._view.model().columnCount() == expected
 
 
-@pytest.mark.skip(reason='Todavía no necesito estos tests')
-@pytest.mark.gui
-@pytest.mark.parametrize(
-    'relation_name, header, tuples',
-    [
-        ('prueba', ['a', 'b'], {('1', '2'), ('2', '1')}),
-        ('persona', ['id', 'name'], {('1', 'gabo'), ('2', 'rodrigo')}),
-    ]
-)
-def test_create_new(qtbot, dialog, relation_name, header, tuples):
-    qtbot.addWidget(dialog)
-    dialog._line_relation_name.setText(relation_name)
-    with mock.patch('pireal.gui.main_window.Pireal.get_service'):
-        view = dialog._view
-        for e, i in enumerate(header):
-            view.horizontalHeader().model().setHeaderData(e, 1, i)
-        for row, t in enumerate(tuples):
-            for col, a in enumerate(t):
-                item = QStandardItem(a)
-                view.model().setItem(row, col, item)
-        dialog._create()
-        robject, rname = dialog._data
-        assert rname == relation_name
-        assert robject.header == header
-        assert robject.content == tuples
+# @pytest.mark.skip(reason='Todavía no necesito estos tests')
+# @pytest.mark.gui
+# @pytest.mark.parametrize(
+#     'columns_to_insert, to_delete, expected',
+#     [
+#         (0, 1, 2),
+#         (3, 1, 4),
+#         (10, 6, 6)
+#     ]
+# )
+# def test_delete_column(qtbot, dialog, columns_to_insert, to_delete, expected):
+#     qtbot.addWidget(dialog)
+#     for _ in range(columns_to_insert):
+#         dialog._NewRelationDialog__add_column()
+#     for _ in range(to_delete):
+#         dialog._NewRelationDialog__delete_column()
+
+#     assert dialog._view.model().columnCount() == expected
+
+
+# @pytest.mark.skip(reason='Todavía no necesito estos tests')
+# @pytest.mark.gui
+# @pytest.mark.parametrize(
+#     'relation_name, header, tuples',
+#     [
+#         ('prueba', ['a', 'b'], {('1', '2'), ('2', '1')}),
+#         ('persona', ['id', 'name'], {('1', 'gabo'), ('2', 'rodrigo')}),
+#     ]
+# )
+# def test_create_new(qtbot, dialog, relation_name, header, tuples):
+#     qtbot.addWidget(dialog)
+#     dialog._line_relation_name.setText(relation_name)
+#     with mock.patch('pireal.gui.main_window.Pireal.get_service'):
+#         view = dialog._view
+#         for e, i in enumerate(header):
+#             view.horizontalHeader().model().setHeaderData(e, 1, i)
+#         for row, t in enumerate(tuples):
+#             for col, a in enumerate(t):
+#                 item = QStandardItem(a)
+#                 view.model().setItem(row, col, item)
+#         dialog._create()
+#         robject, rname = dialog._data
+#         assert rname == relation_name
+#         assert robject.header == header
+#         assert robject.content == tuples
