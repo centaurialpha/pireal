e214ad8dfb5ae81fd1d2d80011327a0871a01488
Gabriel Acosta <acostadariogabriel@gmail.com>
2021-04-09 19:50:07 -0300

    fix: setup.py and deb

--- setup.py
+++ setup.py
@@ -18,62 +18,15 @@
 # along with Pireal; If not, see <http://www.gnu.org/licenses/>.
 
 import os
-from distutils.command.install import install
-from distutils.core import setup
+import shutil
+from setuptools.command.install import install
+from setuptools import setup, find_packages
 
 
 class CustomInstall(install):
-    """
-    Custom installation class on package files.
-
-    It copies all the files into the "PREFIX/share/pireal" dir.
-    """
 
     def run(self):
         install.run(self)
-
-        for script in self.distribution.scripts:
-            script_path = os.path.join(self.install_scripts,
-                                       os.path.basename(script))
-            with open(script_path, 'r') as f:
-                content = f.read()
-            content = content.replace('@ INSTALLED_BASE_DIR @',
-                                      self._custom_data_dir)
-            with open(script_path, 'w') as f:
-                f.write(content)
-
-            src_desktop = self.distribution.get_name() + '.desktop'
-            src_desktop = src_desktop.lower()
-
-            if not os.path.exists(self._custom_apps_dir):
-                os.makedirs(self._custom_apps_dir)
-            dst_desktop = os.path.join(self._custom_apps_dir, src_desktop)
-            with open(src_desktop, 'r') as f:
-                content = f.read()
-            icon = os.path.join(self._custom_data_dir, 'src', 'images',
-                                'pireal_icon.png')
-            content = content.replace('@ INSTALLED_ICON @', icon)
-            with open(dst_desktop, 'w') as f:
-                f.write(content)
-
-    def finalize_options(self):
-        """ Alter the installation path """
-
-        install.finalize_options(self)
-        data_dir = os.path.join(self.prefix, "share",
-                                self.distribution.get_name())
-        apps_dir = os.path.join(self.prefix, "share", "applications")
-
-        if self.root is None:
-            build_dir = data_dir
-        else:
-
-            build_dir = os.path.join(self.root, data_dir[1:])
-            apps_dir = os.path.join(self.root, apps_dir[1:])
-
-        self.install_lib = build_dir
-        self._custom_data_dir = data_dir
-        self._custom_apps_dir = apps_dir
 
 
 classifiers = [
@@ -90,31 +43,18 @@ classifiers = [
 
 ]
 
-DESCRIPTION = ""
 
 setup(
     name="pireal",
     version="3.0",
-    description="",
+    description="Relational Algebra Interpreter",
     author="Gabriel Acosta",
     author_email="acostadariogabriel@gmail.com",
     url="http://centaurialpha.github.io/pireal",
     license='GPLv3+',
     long_description=open('README.md').read(),
-    package_data={
-        "src": [
-            "images/pireal_icon.png",
-            "lang/*.qm"]
-    },
-    packages=[
-        "pireal",
-        "pireal.core",
-        "pireal.core.interpreter",
-        "pireal.gui",
-        "pireal.gui.dialogs",
-        "pireal.gui.query_container"
-    ],
+    packages=find_packages(exclude=['tests']),
     scripts=['bin/pireal'],
     classifiers=classifiers,
-    # cmdclass={'install': CustomInstall},
+    cmdclass={'install': CustomInstall},
 )
